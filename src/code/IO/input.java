8e82a95d5707cfecf18c1641c1a45cc153d27f36
Zack <zjsalim@buffalo.edu>
2017-09-30 15:49:32 -0400

    classes for the circuits input and out values. Each gave getters and setters for the values they hold

---
+++ src/code/IO/input.java
@@ -0,0 +1,13 @@
+package code.IO;
+
+public class input {
+	private int value;
+
+	public void setInputValue(int newValue) {
+		value = newValue;
+	}
+	
+	public int getInputValue() {
+		return value;
+	}
+}
---
+++ src/code/IO/output.java
@@ -0,0 +1,13 @@
+package code.IO;
+
+public class output {
+	private int value;
+	
+	public void setOutputValue(int newValue) {
+		value = newValue;
+	}
+	
+	public int getOutputValue() {
+		return value;
+	}
+}
