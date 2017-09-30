5998c2f3552a2d6a127d4a02b53c0bd398139117
Zack <zjsalim@buffalo.edu>
2017-09-30 16:48:14 -0400

    Added methods for keeping track of input's child objects and which input is connected and the parent which is driving the output. All with comments

--- src/code/IO/input.java
+++ src/code/IO/input.java
@@ -1,9 +1,12 @@
 package code.IO;
 /*Class defining the input "gate" of the circuits. Input (1 or 0) of each 
  * input line in the circuit is held here.*/
+import java.util.ArrayList;
 
 public class input {
 	private int value;
+	private ArrayList<Object> childrenInputs1 = new ArrayList<Object>();
+	private ArrayList<Object> childrenInputs2 = new ArrayList<Object>();
 	
 	/*setter for the input value.*/
 	public void setInputValue(int newValue) {
@@ -14,4 +17,25 @@ public class input {
 	public int getInputValue() {
 		return value;
 	}
+	/*Adds child gate object to array list signifying that
+	 * the input is going to the 1st input of the gate*/
+	public void addChild_Input1(Object newChild) {
+		childrenInputs1.add(newChild);
+	}
+	/*Adds child gate object to array list signifying that
+	 * the input is going to the 2nd input of the gate*/
+	public void addChild_Input2(Object newChild) {
+		childrenInputs2.add(newChild);
+	}
+	/*Gets the ArrayList of objects where the input
+	 * is connected to the 1st input of the child gate*/
+	public ArrayList<Object> getInput1Children() {
+		return childrenInputs1;
+	}
+	/*Gets the ArrayList of objects where the input
+	 * is connected to the 2nd input of the child gate*/
+	public ArrayList<Object> getInput2Children() {
+		return childrenInputs2;
+	}
+	
 }
--- src/code/IO/output.java
+++ src/code/IO/output.java
@@ -4,6 +4,7 @@ package code.IO;
 
 public class output {
 	private int value;
+	private Object inputSource = null;
 	
 	/*setter for the output value.*/
 	public void setOutputValue(int newValue) {
@@ -13,5 +14,20 @@ public class output {
 	/*getter for the output value.*/
 	public int getOutputValue() {
 		return value;
+	}
+	/*sets the input Object gate that is setting the value of the output
+	 * Returns 4 if successful and 0 otherwise*/
+	public int setInput(Object newInputSource) {
+		if (inputSource == null){
+			inputSource = newInputSource;
+			return 4;
+		}
+		else {
+			return 0;
+		}
+	/*Returns the object that is setting the input*/	
+	}
+	public Object getInput() {
+		return inputSource;
 	}
 }
