9d45d07276951db55ebcb82e872b5e74b738acb6
Zack <zjsalim@buffalo.edu>
2017-09-30 15:54:05 -0400

    added comments for the classes and methods

--- src/code/IO/input.java
+++ src/code/IO/input.java
@@ -1,12 +1,16 @@
 package code.IO;
+/*Class defining the input "gate" of the circuits. Input (1 or 0) of each 
+ * input line in the circuit is held here.*/
 
 public class input {
 	private int value;
-
+	
+	/*setter for the input value.*/
 	public void setInputValue(int newValue) {
 		value = newValue;
 	}
 	
+	/*getter for the input value.*/
 	public int getInputValue() {
 		return value;
 	}
--- src/code/IO/output.java
+++ src/code/IO/output.java
@@ -1,12 +1,16 @@
 package code.IO;
+ /*Class defining the output "gate" of the circuits. Output (1 or 0) of each 
+  * output line in the circuit is held here.*/
 
 public class output {
 	private int value;
 	
+	/*setter for the output value.*/
 	public void setOutputValue(int newValue) {
 		value = newValue;
 	}
 	
+	/*getter for the output value.*/
 	public int getOutputValue() {
 		return value;
 	}
